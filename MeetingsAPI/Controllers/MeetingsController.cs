a803d0260177db6e47dd7923412464384d936307
Xansher <grzesiekaszlar@gmail.com>
2021-05-26 23:26:45 +0200

    Added needed packages

---
+++ MeetingsAPI/Controllers/MeetingsController.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using MeetingsAPI.DTOs;
+using MeetingsAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeetingsAPI.Controllers
+{
+    [ApiController]
+    [Route("meetings")]
+    public class MeetingsController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+
+        public MeetingsController(ApplicationDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<MeetingDTO>>> Get()
+        {
+            var meetings = await context.Meetings.OrderBy(x => x.Date)
+                .Include(x => x.MeetingsMembers).ThenInclude(x => x.Member)
+                .ToListAsync(); ;
+            return mapper.Map<List<MeetingDTO>>(meetings);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<MeetingDTO>> Get(int id)
+        {
+            var meeting = await context.Meetings
+                .Include(x => x.MeetingsMembers).ThenInclude(x => x.Member)
+                .FirstOrDefaultAsync(x => x.Id == id);
+            if (meeting == null)
+                return NotFound();
+
+            return mapper.Map<MeetingDTO>(meeting);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] MeetingCreatingDTO meetingCreatingDTO)
+        {
+            var meeting = mapper.Map<Meeting>(meetingCreatingDTO);
+            context.Add(meeting);
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpPost("join/{id:int}")]
+        public async Task<ActionResult> AddMember(int id, [FromBody] MemberCreatingDTO memberDTO)
+        {
+            var meetingResult = await Get(id);
+            if (meetingResult.Result is NotFoundResult)
+                return NotFound("Not found meeting");
+
+            if (meetingResult.Value.Members.Count >= 25)
+                return Content("Meeting is full");
+
+            var member = mapper.Map<Member>(memberDTO);
+            context.Add(member);
+    
+            
+            context.SaveChanges();
+            context.MeetingsMembers.Add(new MeetingsMembers() { MeetingId = id, MemberId = member.Id });
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var meeting = await context.Meetings
+                .Include(x => x.MeetingsMembers).ThenInclude(x => x.Member)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (meeting == null)
+                return NotFound();
+
+            context.Remove(meeting);
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ MeetingsAPI/Migrations/20210526110005_Initial.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MeetingsAPI.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Meetings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Meetings", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MeetingsMembers",
+                columns: table => new
+                {
+                    MeetingId = table.Column<int>(type: "int", nullable: false),
+                    MemberId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MeetingsMembers", x => new { x.MemberId, x.MeetingId });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Members",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Members", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Meetings");
+
+            migrationBuilder.DropTable(
+                name: "MeetingsMembers");
+
+            migrationBuilder.DropTable(
+                name: "Members");
+        }
+    }
+}
