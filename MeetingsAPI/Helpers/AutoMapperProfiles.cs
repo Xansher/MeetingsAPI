e5de02369d35f0b698e79d06a528474d7e21d5ff
Xansher <grzesiekaszlar@gmail.com>
2021-05-26 23:25:59 +0200

    Created automapper profiles

---
+++ MeetingsAPI/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using MeetingsAPI.DTOs;
+using MeetingsAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeetingsAPI.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<MeetingCreatingDTO, Meeting>();
+            CreateMap<Meeting, MeetingDTO>().ForMember(x => x.Members, options => options.MapFrom(MapMeetingMembers)).ReverseMap();
+            CreateMap<MemberCreatingDTO, Member>();
+        }
+
+        private List<MemberDTO> MapMeetingMembers(Meeting meeting, MeetingDTO meetingDTO)
+        {
+            var result = new List<MemberDTO>();
+            if(meeting.MeetingsMembers != null)
+            {
+                foreach (var member in meeting.MeetingsMembers)
+                {
+                    result.Add(new MemberDTO()
+                    {
+                        Id = member.MemberId,
+                        Name = member.Member.Name,
+                        Email = member.Member.Email
+                    });
+                }
+            }
+            return result;
+        }
+    }
+}
