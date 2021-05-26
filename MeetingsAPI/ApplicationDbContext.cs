c9463b1b28943a9c43ff8ef172fe926ef4d811f4
Xansher <grzesiekaszlar@gmail.com>
2021-05-26 23:23:25 +0200

    Created db context

---
+++ MeetingsAPI/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using MeetingsAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeetingsAPI
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<MeetingsMembers>().HasKey(x => new { x.MemberId , x.MeetingId });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Meeting> Meetings { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<MeetingsMembers> MeetingsMembers { get; set; }
+
+    }
+}
