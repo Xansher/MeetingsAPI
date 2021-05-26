40ec91f86372b06379407b4413e7ca8400024fdd
Xansher <grzesiekaszlar@gmail.com>
2021-05-26 23:22:22 +0200

    Created MeetingsMembers entity

---
+++ MeetingsAPI/Entities/MeetingsMembers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeetingsAPI.Entities
+{
+    public class MeetingsMembers
+    {
+        public int MeetingId { get; set; }
+        public int MemberId { get; set; }
+        public Member Member { get; set; }
+    }
+}
